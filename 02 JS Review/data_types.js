968bd773578548a9b2ee40b08b73e660f0f4a676
gemstone-source <sophiajulius5@gmail.com>
2024-07-09 00:47:49 +0300

    Data types

---
+++ 02 JS Review/data_types.js
@@ -0,0 +1,12 @@
+//function declaration
+function great() {
+  console.log("Hello There");
+}
+
+//function expression
+const speak = function () {
+  console.log("Speak");
+};
+
+speak();
+great();
