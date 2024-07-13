5769c490a47a8243028dba2ac60c3410c2d42112
gemstone-source <sophiajulius5@gmail.com>
2024-07-13 23:16:17 +0300

    asynchronous

---
+++ 02 JS Review/asynchro.js
@@ -0,0 +1,23 @@
+const getTodos = (callback) => {
+  const request = new XMLHttpRequest();
+
+  request.addEventListener("readystatechange", () => {
+    if (request.readyState === 4 && request.status === 200) {
+      callback(undefined, request.responseText);
+    } else if (request.readyState === 4) {
+      callback("Could not fetch data", undefined);
+    }
+  });
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos");
+  request.send();
+};
+
+getTodos((err, data) => {
+  console.log("Callback fired");
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
