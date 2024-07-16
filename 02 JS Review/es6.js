bf3522763873403cdf363bd1820c827625ed947f
gemstone-source <sophiajulius5@gmail.com>
2024-07-17 00:35:54 +0300

    es6 plus

--- 02 JS Review/es6.js
+++ 02 JS Review/es6.js
@@ -1,8 +1,21 @@
 //Template Literals
-let word1 ="java"
-let word2 ="script"
+let word1 = "java";
+let word2 = "script";
 
-const fullName = `${word1} ${word2}`
+const fullName = `${word1} ${word2}`;
+console.log(fullName);
 
+const personInformation = {
+  firstName: "John",
+  lastName: "Doe",
+  city: "DSM",
+  tribe: "Chagga",
+  religion: "Christian",
+};
 
-console.log(fullName)
+// re assign new names on long variables
+const {firstName,lastName}= personInformation
+console.log(`${firstName} ${lastName}`)
+
+const {firstName:fn, lastName:ln} = personInformation
+console.log(`${fn} ${ln}`)
